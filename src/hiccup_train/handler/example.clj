67b9416ca936f6bd83697cfa37291406c90b863a
kero_hirake <k16.h.white@gmail.com>
2022-04-24 13:11:02 +0900

    add reitit

--- src/hiccup_train/handler/example.clj
+++ src/hiccup_train/handler/example.clj
@@ -1,9 +1,20 @@
 (ns hiccup-train.handler.example
-  (:require [compojure.core :refer :all]
-            [clojure.java.io :as io]
-            [integrant.core :as ig]))
+  (:require   [integrant.core :as ig]
+              [reitit.ring :as ring]
+              [muuntaja.core :as m]
+              [reitit.ring.middleware.muuntaja :as muuntaja]
+              [ring.util.response :as res]
+              [reitit.ring.middleware.parameters :as params]))
+
+(defn ok [_]
+  (-> "ok"
+      (res/response)
+      (res/content-type  "text/html; charset=utf-8")))
 
 (defmethod ig/init-key :hiccup-train.handler/example [_ options]
-  (context "/example" []
-    (GET "/" []
-      (io/resource "hiccup_train/handler/example/example.html"))))
+  (ring/ring-handler
+   (ring/router
+    [["/" ok]]
+    {:data {:muuntaja m/instance
+            :middleware [muuntaja/format-middleware
+                         params/parameters-middleware]}})))
